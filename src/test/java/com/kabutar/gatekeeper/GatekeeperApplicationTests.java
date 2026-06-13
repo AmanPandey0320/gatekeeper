a330af81417c0eb447e879ab2c3bc008d8f23efe
Aman Kr Pandey <aman.pandey.corp@gmail.com>
2026-06-13 18:08:01 +0530

    added java 26

--- src/test/java/com/kabutar/gatekeeper/GatekeeperApplicationTests.java
+++ src/test/java/com/kabutar/gatekeeper/GatekeeperApplicationTests.java
@@ -3,7 +3,7 @@ package com.kabutar.gatekeeper;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = GatekeeperApplication.class)
 class GatekeeperApplicationTests {
 
 	@Test
